0b7e42c3694555ff6b33d8938e70cc2c27245cb3
wplb <3438637988@qq.com>
2022-08-30 19:17:24 +0800

    添加判断应用是否在前台运行

--- LBFloatUniBridging/src/main/java/com/lonbon/floatunibridging/AppStartReceiver.java
+++ LBFloatUniBridging/src/main/java/com/lonbon/floatunibridging/AppStartReceiver.java
@@ -1,10 +1,13 @@
 package com.lonbon.floatunibridging;
 
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.util.Log;
+
+import java.util.List;
 
 /**
  * *****************************************************************************
@@ -36,8 +39,27 @@ public class AppStartReceiver extends BroadcastReceiver {
      * @param packageName
      */
     private void startActivity(Context context,String packageName){
+        if (isRunningAppProcesses(context,packageName)){
+            return;
+        }
         PackageManager packageManager = context.getPackageManager();
         Intent it = packageManager.getLaunchIntentForPackage(packageName);
         context.startActivity(it);
     }
+    
+    public boolean isRunningAppProcesses(Context context, String packageName) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> appProcesses = activityManager.getRunningAppProcesses();
+        if (appProcesses == null) {
+            return false;
+        }
+        for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
+            if (appProcess.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND && appProcess.processName.equals(packageName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
